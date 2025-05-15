cac79d68592e3a0bc68f05647e15a3fa323d46c4
Abhishek <abhishekrajput4736@gmail.com>
2025-05-15 15:26:29 +0530

    updated index.js

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from "react";
+import Navbar from "../components/Navbar";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useFetch from "../useFetch";
+import { AuthContext } from "../authContext";
+import "../styles/home.css";
+import Card from "../components/Card";
+
+const Home = () => {
+  const [query, setQuery] = useState("");
+  const { user } = useContext(AuthContext);
+  const { data, loading } = useFetch(`/entries/author/${user._id}`);
+
+  const keys = ["title", "location", "date"];
+
+  // const search = (data) => {
+  //     return data.filter((item) =>
+  //         keys.some((key) => item[key] &&
+  //             item[key].toLowerCase().includes(query))
+  //     );
+  // };
+  const search = (data) => {
+    const lowerQuery = query.toLowerCase();
+    return data.filter((item) =>
+      keys.some(
+        (key) => item[key] && item[key].toLowerCase().includes(lowerQuery)
+      )
+    );
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="search">
+        <div className="searchBar">
+          <h2>Explore</h2>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search places or dates"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
+          </div>
+        </div>
+      </div>
+
+      <div className="searchedPosts">
+        {loading ? (
+          <>
+            <div
+              className="p"
+              style={{
+                color: "white",
+                fontFamily: "'Kaushan Script', cursive",
+              }}
+            >
+              Loading...
+            </div>
+          </>
+        ) : (
+          <>
+            {search(data)?.map((item) => (
+              <Card
+                key={item._id}
+                _id={item._id}
+                photos={item.photos}
+                title={item.title}
+                date={item.date}
+                location={item.location}
+                text={item.text}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
